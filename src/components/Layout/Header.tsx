b0a67f4ca26db9bbacf34ad3ffcf5d3a0bc3def1
Lee-Yeseul <ssulv3030@gmail.com>
2023-11-23 16:39:06 +0900

    feature : searchBar component 추가

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,11 @@
+import SearchBar from "./SearchBar";
+
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full h-16 border-solid border-b-[1px] border-gray-200 z-10 bg-white">
       <div className="h-full flex items-center px-8 justify-between">
         <div>로고</div>
-        <div>search bar</div>
+        <SearchBar />
         <div>로그인</div>
       </div>
     </header>
---
+++ src/components/Layout/SearchBar.tsx
@@ -0,0 +1,32 @@
+// import { ReactComponent as SearchIcon } from "@src/assets/icons/search.svg";
+import SearchIcon from "@src/assets/icons/search.svg?react";
+import { FormEvent, useRef } from "react";
+
+export default function SearchBar() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!searchInputRef.current) return;
+    console.log(searchInputRef.current.value);
+  };
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <div className="flex items-center justify-between pl-6 pr-3 w-96 h-11 rounded-full shadow-md border-solid border-gray-300 border-[1px]">
+        <input
+          ref={searchInputRef}
+          className="h-full w-11/12 hover:border-none focus:outline-none"
+          placeholder="검색어를 입력하세요."
+        />
+        <button type="submit">
+          <SearchIcon
+            width="32"
+            height="32"
+            fill="white"
+            className="rounded-full bg-pink-200 p-1"
+          />
+        </button>
+      </div>
+    </form>
+  );
+}
