8a757664c7d11ad7730c8b2fb106fd4f105991d4
Lee-Yeseul <ssulv3030@gmail.com>
2024-01-06 22:38:43 +0900

    feature : kakao login 실패시 에러 처리 추가

--- src/pages/KakaoRedirectPage/index.tsx
+++ src/pages/KakaoRedirectPage/index.tsx
@@ -1,12 +1,14 @@
 import { useKakaoLoginCodeMutation } from "@src/hooks/querys/auth";
 import { SignInResponse } from "@src/types/auth";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function KakaoRedirect() {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const [isLoginSuccess, setIsLoginSuccess] = useState(false);
   const code = searchParams.get("code");
+  const error = searchParams.get("error");
 
   const handleSuccess = (data: SignInResponse) => {
     console.log(data);
@@ -18,10 +20,13 @@ export default function KakaoRedirect() {
   const { mutate } = useKakaoLoginCodeMutation(handleSuccess, handleError);
 
   useEffect(() => {
+    if (!code && error) {
+      navigate("/dashboard");
+    }
     if (!isLoginSuccess && code) {
       mutate(code);
+      setIsLoginSuccess(true);
     }
-    setIsLoginSuccess(true);
   }, []);
 
   return <></>;
